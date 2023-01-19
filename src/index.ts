bb28e80c495cc0355ca5776844c34f832979d3b6
JoaoPaulo-creator <joaosarto@gmail.com>
2023-01-18 22:48:09 -0300

    Adicionar mais um header na config do cors

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,15 @@ import router from './routes'
 
 const app = express()
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', '*')
-  res.setHeader('Access-Control-Allow-Headers', '*')
+app.use((req, response, next) => {
+  response.setHeader('Access-Control-Allow-Origin', '*')
+  response.setHeader('Access-Control-Allow-Methods', '*')
+	response.setHeader('Access-Control-Allow-Headers', '*')
+  response.setHeader('Access-Control-Max-Age', '20')
 
   next()
 })
+
 app.use(express.json())
 app.use(router)
 
